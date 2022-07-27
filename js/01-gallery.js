2cbfbc421b4e37f3e72aaee4777c326e2a593064
KutsevEA <kucev.ea@gmail.com>
2022-07-27 20:58:04 +0300

    HW#7 Done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.addEventListener('click', showFullScreenPicture);
+
+const markupForGallery = createMarkupForGallery(galleryItems);
+
+function createMarkupForGallery(items) {
+  return items
+    .map(
+      item =>
+        `<div class="gallery__item"><a class="gallery__link" href="${item.original}"><img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}"/></a></div>`
+    )
+    .join(``);
+}
+
+galleryEl.insertAdjacentHTML('beforeend', markupForGallery);
+
+function showFullScreenPicture(evt) {
+    evt.preventDefault();
+
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `
+    <div class="modal">
+        <img class="gallery__image" src="${evt.target.dataset.source}" data-source="${evt.target.dataset.source}" alt="${evt.target.alt}"/>
+    </div>
+`,
+    {
+      onClose: instance => {
+        document.removeEventListener('keydown', waitingForEscape);
+      }
+    }
+  );
+  instance.show();
+
+  document.addEventListener('keydown', waitingForEscape);
+
+  function waitingForEscape(evt) {
+    if (evt.code === 'Escape') {
+      instance.close();
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+const markupForGallery = createMarkupForGallery(galleryItems);
+
+function createMarkupForGallery(items) {
+  return items
+    .map(
+      item =>
+        `<li><a class="gallery__item" href="${item.original}"><img class="gallery__image" src="${item.preview}" alt="${item.description}" /></a></li>`
+    )
+    .join(``);
+}
+
+galleryEl.insertAdjacentHTML('beforeend', markupForGallery);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionType: 'attr',
+  captionDelay: 250,
+});
+
+gallery.on('show.simplelightbox');
